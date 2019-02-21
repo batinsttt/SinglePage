cb3a0f5bcdb9035cb1e9b4609eaae75df2928bdc
os-sinhtv <os-sinhtv@OS-SINHTV.ViettelGroup.ViettelAD.com>
2019-02-21 08:29:42 +0700

    SinhTV

--- SinglePageDemo/src/main/java/com/sttt/ruby/controller/CallAjaxController.java
+++ SinglePageDemo/src/main/java/com/sttt/ruby/controller/CallAjaxController.java
@@ -4,26 +4,32 @@ package com.sttt.ruby.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+
 @RestController
 public class CallAjaxController {
 
 	/*
-	 * Demo web service
+	 * Demo Post service
 	*/	
 	@PostMapping("/service/call")
-	public String callWebservice(@RequestBody String address) throws Exception {
+	public String callWebservice(@RequestBody String requestBody) throws Exception {
 		String uri ="http://10.30.176.198:9006/ITSolWebService/sales/edit";
-		Map<String, String> variables = new HashMap<String, String>(3);
-        variables.put("address", address);
-        variables.put("id", "10000009");
         RestTemplate restTemplate = new RestTemplate();
-        String json =  restTemplate.getForObject(uri, String.class, variables);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> entity = new HttpEntity<String>(requestBody,headers);
+        String json = restTemplate.postForObject(uri, entity, String.class);
+        
 		System.out.println(json);
 		
 		return json;
